f1f4432aae320e43ff7d974462f7da362d278644
BhargaviGottumukkala <bhargavigottumukkala19@gmail.com>
2024-10-11 15:31:44 +0530

    Initial commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstTest {
+	public static void main(String args[]) {
+	WebDriverManager.chromedriver().setup();
+	ChromeDriver driver=new ChromeDriver();
+	driver.get("https://www.google.com");
+}
+}
